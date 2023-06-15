34b7d43f025e68cf4432a641833747b764f51ea3
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-06-15 20:42:26 +0000

    Initial commit

---
+++ front/articles.php
@@ -0,0 +1,4 @@
+<div>
+  <h3><?php echo $title;?> - <?php echo $price; ?></h3>
+  <p><?php echo $Description; ?></p>
+</div>
---
+++ index.php
@@ -0,0 +1,71 @@
+<?php
+ $language = 'fr';
+?>
+
+
+<html>
+  <head>
+    <title>Ma boutique en ligne</title>
+  </head>
+  <body>
+    <header>
+      <h1>
+        <?php
+        if($language === 'fr')
+        {
+          echo 'Bienvenue !';
+        } else {
+          echo 'Welcome !' ;        
+        }
+        ?>
+      </h1>
+    </header>
+    <div>
+      <h2>
+        <?php
+        if($language === 'fr')
+        {
+          echo 'Nos articles' ;
+        }else{
+          echo 'Our products !';
+        }
+        ?>:
+      </h2>
+      <?php
+        function loadArticles (): array 
+          {
+            return [
+              [
+              'Name' => 'Carte mère' ,
+              'Price'=> 120 ,
+              'Description' => 'Une super carte mère !',
+              ],
+              [
+              'Name' => 'Carte graphique',
+              'Price'=> 500 ,
+              'Description' => 'A vous le 4K', 
+              ],
+              [
+              'Name' => 'Barrettes de RAM',
+              'Price'=> 150 ,
+              'Description' => '16Go DDR4',
+              ],
+            ];
+          }
+
+          foreach (loadArticles() as $article)
+            {
+              echo '<div>
+                     <h3> '. $article['Name']. ' - '. $article['Price'].'€</h3>
+                     <p>'.$article['Description'].'</p>
+                    </div>';
+            }
+      ?>
+    </div>
+    <?php
+?> 
+
+    
+  <script src="https://replit.com/public/js/replit-badge-v2.js" theme="dark" position="bottom-right"></script>
+  </body>
+</html>
